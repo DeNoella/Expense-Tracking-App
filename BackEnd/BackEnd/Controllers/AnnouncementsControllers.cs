04fbdabb741f95b2a340bce12ab67e4adb21728d
DeNoella <angenoella1771@gmail.com>
2025-12-12 18:01:54 +0200

    Created announcement controllers file

---
+++ BackEnd/BackEnd/Controllers/AnnouncementsControllers.cs
@@ -0,0 +1,8 @@
+using BackEnd.Authorization;
+using BackEnd.Data;
+using BackEnd.Entities;
+using BackEnd.Models.Announcements;
+using BackEnd.Models.Mappers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
