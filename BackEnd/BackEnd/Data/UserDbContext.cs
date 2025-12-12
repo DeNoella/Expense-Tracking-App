a21b167ee2fcd1716b6cc343675e2553b1eee9e4
DeNoella <angenoella1771@gmail.com>
2025-12-12 22:38:33 +0200

    Add BuyPointDbContext with DbSets for all entities

---
+++ BackEnd/BackEnd/Data/UserDbContext.cs
@@ -0,0 +1,18 @@
+using BackEnd.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Data
+{
+    public class BuyPointDbContext(DbContextOptions<BuyPointDbContext> options) : DbContext(options)
+    {
+        public DbSet<Users> Users => Set<Users>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Cart> Carts => Set<Cart>();
+        public DbSet<CartItem> CartItems => Set<CartItem>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+        public DbSet<Payment> Payments => Set<Payment>();
+        public DbSet<Issue> Issues => Set<Issue>();
+        public DbSet<Discount> Discounts => Set<Discount>();
+        public DbSet<Announcement> Announcements => Set<Announcement>();
+        public DbSet<Image> Images => Set<Image>();
