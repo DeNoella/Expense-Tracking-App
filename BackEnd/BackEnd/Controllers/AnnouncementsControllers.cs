3f4761ed9150c033ff627d54a5b60930b705d764
DeNoella <angenoella1771@gmail.com>
2025-12-12 18:08:25 +0200

    AnnouncementsController with dependency injection forPointDbContext

--- BackEnd/BackEnd/Controllers/AnnouncementsControllers.cs
+++ BackEnd/BackEnd/Controllers/AnnouncementsControllers.cs
@@ -6,3 +6,17 @@ using BackEnd.Models.Mappers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AnnouncementsController : ControllerBase
+    {
+        private readonly BuyPointDbContext _db;
+
+        public AnnouncementsController(BuyPointDbContext db)
+        {
+            _db = db;
+        }
+}
